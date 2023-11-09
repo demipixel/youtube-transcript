b675acd7283318c54f5f8307ca2ed0e2c4a51d66
Lucas Simon <luke5227@gmail.com>
2023-11-08 17:52:02 -0800

    Include /dist because I can't be bothered to make an npm module

---
+++ dist/index.d.ts
@@ -0,0 +1,40 @@
+import * as Axios from 'axios';
+export declare class YoutubeTranscriptError extends Error {
+    constructor(message: any);
+}
+export interface TranscriptConfig {
+    lang?: string;
+    country?: string;
+    AxiosClient?: Axios.AxiosInstance;
+}
+export interface TranscriptResponse {
+    text: string;
+    duration: number;
+    offset: number;
+}
+/**
+ * Class to retrieve transcript if exist
+ */
+export declare class YoutubeTranscript {
+    /**
+     * Fetch transcript from YTB Video
+     * @param videoId Video url or video identifier
+     * @param config Get transcript in another country and language ISO
+     */
+    static fetchTranscript(videoId: string, config?: TranscriptConfig): Promise<TranscriptResponse[]>;
+    /**
+     * Generate tracking params for YTB API
+     * @param page
+     * @param config
+     */
+    private static generateRequest;
+    /**
+     *  'base.js' function
+     */
+    private static generateNonce;
+    /**
+     * Retrieve video id from url or string
+     * @param videoId video url or video id
+     */
+    private static retrieveVideoId;
+}
